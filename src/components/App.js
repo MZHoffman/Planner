db5665d6ce4dcddf5c4aea20f8703fc4f4f348f5
Michal Hoffman <michal@Michals-MacBook-Pro.local>
2018-03-28 22:16:32 +0100

    moving fixUserInput to functions.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,66 +2,35 @@ import React from 'react';
 import Header from './Header';
 import Event from './Event';
 import sampleEvents from '../sample-events';
+import { timeMath } from '../functions'
 
 class App extends React.Component {
   state = {
     events: []
   }
-  /*
-    sortByTime = (events) => {
-      return events.sort((a, b) => (a.timestamp - b.timestamp));
-    }
-  */
+
   loadSampleEvents = () => {
-    // console.log(sampleEvents)
-    //const sortedEvents = this.sortByTime(sampleEvents); //why it doesnt work?
     sampleEvents.sort((a, b) => (a.timestamp - b.timestamp));
     this.setState({ events: sampleEvents });
   }
-  timeStampTo24hFormat = (timeStamp) => {
-    let date = new Date(timeStamp);
-    let hours = "0" + date.getHours();
-    let minutes = "0" + date.getMinutes();
-    let formattedTime = hours.substr(-2) + ':' + minutes.substr(-2);
-    return formattedTime
-  }
-  timeToTimestamp = HHmm => { return Date.parse(`1970-1-1 ${HHmm}`) }
+
   updateEvent = (key, updatedEvent) => {
-    // console.log(this.state.events);
     const events = [...this.state.events];
     events[key] = updatedEvent;
     this.setState({ events });
   }
   tidyUpTime = (key, transformedTime, fieldName) => {
-    console.log(fieldName)
     const events = [...this.state.events];
     events[key] = transformedTime;
     events.sort((a, b) => (a.timestamp - b.timestamp));
     if (fieldName === "end") {
-      events.map(e =>
-        (e.duration = this.timeStampTo24hFormat
-          (
-          this.timeToTimestamp(e.end) -
-          this.timeToTimestamp(e.start)
-          )
-        )
-      )
+      events.map(e => (e.duration = timeMath(e.end, e.start, "sub")))
     }
-    events.map(e =>
-      (e.end = this.timeStampTo24hFormat
-        (
-        this.timeToTimestamp(e.start) +
-        this.timeToTimestamp(e.duration)
-        )
-      )
-    )
+    events.map(e => (e.end = timeMath(e.start, e.duration, "add")))
+
     this.setState({ events });
-    // () => this.sortByTime;
   }
   render() {
-    //this.timeStampTo24hFormat(232323232);
-    //this.sortByTime(this.state.events)
-    //console.log("sortByTime");
     return (
       <div>
         <Header subject="Internet Safety" />
--- src/functions.js
+++ src/functions.js
@@ -1,6 +1,20 @@
-export function sorting(events) => {
-    console.log("sorting!!!");
-    const events = [...this.state.events];
-    events.sort((a, b) => (a.timestamp - b.timestamp));
-    this.setState({ events })
+
+
+export function timeToTimestamp(Time) {
+    return Date.parse(`1970-1-1 ${Time}`)
+}
+
+export function timestampToTime(timestamp) {
+    let date = new Date(timestamp);
+    let hours = "0" + date.getHours();
+    let minutes = "0" + date.getMinutes();
+    let formattedTime = hours.substr(-2) + ':' + minutes.substr(-2);
+    return formattedTime
+}
+
+export function timeMath(t1, t2, operation) {
+    t1 = timeToTimestamp(t1);
+    t2 = timeToTimestamp(t2);
+    let result = operation === "add" ? t1 + t2 : operation === "sub" ? t1 - t2 : console.log("add or sub onbly");
+    return timestampToTime(result)
 }
