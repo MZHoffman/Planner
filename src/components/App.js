57d685065563f654325256072a42232eb8feaf71
Michal Hoffman <michal.z.hoffman@gmail.com>
2018-12-23 19:18:09 +0000

    Updating serve component due to vulnerability

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import Header from './Header';
-import Event from './Event';
-import sampleEvents from '../sample-events';
+import React from 'react'
+import Header from './Header'
+import Event from './Event'
+import sampleEvents from '../sample-events'
 import { timeMath, fixUserInput } from '../functions'
 
 class App extends React.Component {
@@ -10,66 +10,68 @@ class App extends React.Component {
   }
 
   loadSampleEvents = () => {
-    sampleEvents.sort((a, b) => (a.timestamp - b.timestamp));
-    this.setState({ events: sampleEvents });
+    sampleEvents.sort((a, b) => a.timestamp - b.timestamp)
+    this.setState({ events: sampleEvents })
   }
 
   updateEvent = (key, updatedEvent) => {
-    const events = [...this.state.events];
-    events[key] = updatedEvent;
-    this.setState({ events });
+    const events = [...this.state.events]
+    events[key] = updatedEvent
+    this.setState({ events })
   }
 
   tidyUpTime = (key, transformedTime) => {
-    //console.log(fieldName, key, transformedTime);
-    console.log(key, transformedTime);
-    console.log('000', this.state.events);
-    const events = [...this.state.events];
-    console.log('111', events);
-    events[key] = transformedTime;
+    // console.log(fieldName, key, transformedTime);
+    console.log(key, transformedTime)
+    console.log('000', this.state.events)
+    const events = [...this.state.events]
+    console.log('111', events)
+    events[key] = transformedTime
     console.log('222', events)
-    //events.sort((a, b) => (a.timestamp - b.timestamp));
-    console.log('333', events);
-    events.map(e => (e.end = timeMath(e.start, e.duration, "add")))
+    // events.sort((a, b) => (a.timestamp - b.timestamp));
+    console.log('333', events)
+    events.map(e => (e.end = timeMath(e.start, e.duration, 'add')))
     events.map((e, key) => {
-      //let prev = event.start;
+      // let prev = event.start;
       if (this.state.events[key - 1]) {
         e.start = this.state.events[key - 1].end
-        //console.log()
+        // console.log()
         console.log(e.start)
       }
     })
-    console.log(events);
-    //events.reduce((events, event, key) => {
+    console.log(events)
+    // events.reduce((events, event, key) => {
     // console.log(event.start, "reduce");
-    //let prev = event.start
-    //if (this.state.events[key - 1]) { prev = this.state.events[key - 1].end }
-    //console.log(events, "events reduce");
-    //console.log(prev, "events reduce");
-    //event.start = prev
-    //return events;
-    //}, [])
-    //console.log(events);
-    //console.log(events, "outside");
+    // let prev = event.start
+    // if (this.state.events[key - 1]) { prev = this.state.events[key - 1].end }
+    // console.log(events, "events reduce");
+    // console.log(prev, "events reduce");
+    // event.start = prev
+    // return events;
+    // }, [])
+    // console.log(events);
+    // console.log(events, "outside");
 
-    this.setState({ events: events });
+    this.setState({ events })
   }
   render() {
-    console.log("render", this.state.events);
+    console.log('render', this.state.events)
     return (
       <div>
-        <Header subject="Internet Safety" />
+        <Header subject='Internet Safety' />
         <button onClick={this.loadSampleEvents}>Load sample events</button>
         <button onClick={this.sortByTime}>sort</button>
-        {Object.keys(this.state.events).map(key => <Event //to rethink why Ocject.keys
-          event={this.state.events[key]}
-          key={key}
-          index={key}
-          updateEvent={this.updateEvent}
-          tidyUpTime={this.tidyUpTime}
-        />)}
+        {Object.keys(this.state.events).map(key => (
+          <Event // to rethink why Ocject.keys
+            event={this.state.events[key]}
+            key={key}
+            index={key}
+            updateEvent={this.updateEvent}
+            tidyUpTime={this.tidyUpTime}
+          />
+        ))}
       </div>
-    );
+    )
   }
 }
-export default App;
+export default App
